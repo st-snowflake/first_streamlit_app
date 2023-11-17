c61d47a44e173e586bc903345a32d45b095498a0
st-snowflake <151125961+st-snowflake@users.noreply.github.com>
2023-11-17 18:30:26 +1100

    Update streamlit_app.py
    Added another Header for Special Item

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ streamlit.text( ' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit. text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
